3b4d2c6595168e42358b1c0146a1449e6ddb901b
Gustavo Pinheiro <gustavo.eng18@gmail.com>
2020-07-18 16:45:36 -0400

    Correção do nome da pasta de resumo e Criando estrutura de controle Switch

---
+++ 4-estruturas-de-programacao/19-switch/script.js
@@ -0,0 +1,13 @@
+let nome = "Gustavo"
+
+switch (nome) {
+    case "Gustavo":
+        console.log("O nome é Gustavo")
+        break
+    case "João":
+        console.log("O nome é João")
+        break
+    default:
+        console.log("O nome não foi encontrado")
+        break
+}
