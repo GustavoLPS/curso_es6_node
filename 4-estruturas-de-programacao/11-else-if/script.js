ef686a14c7740e974b88206a2364f920f8d9b83b
Gustavo Pinheiro <gustavo.eng18@gmail.com>
2020-07-18 15:43:07 -0400

    Estrutura Else If

---
+++ 4-estruturas-de-programacao/11-else-if/script.js
@@ -0,0 +1,16 @@
+let nome = "Gustavo"
+let idade = 28
+
+if (nome != undefined && nome == "Joaquim") {
+    console.log("O nome está definido")
+} else if (nome == "Gustavo" && nome.length > 5 && idade == 50) {
+    console.log("O nome é Gustavo")
+} else {
+    console.log("Não é Gustavo")
+}
+
+if (1 > 2) {
+    console.log("Teste");
+} else if (1 == 1) {
+    console.log("Testando");
+}
