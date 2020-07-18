8c40a4180a1ac14235e1240fcf3186570def9e24
Gustavo Pinheiro <gustavo.eng18@gmail.com>
2020-07-18 14:53:04 -0400

    Convenção de variáveis

---
+++ 4-estruturas-de-programacao/3-convencao/script.js
@@ -0,0 +1,8 @@
+// let 3nome = "teste" erro
+let nome = "teste"
+let $nome = "teste2"
+let _nome = "teste3"
+// let @nome = "teste" erro
+// let !nome = "teste" erro
+let Nome = "teste"
+let seuNome = "teste"
